d3d3e5629db2373e1c9e1ead47917c1a551b1796
Eduardo <110469827+whiteplayboy@users.noreply.github.com>
2024-04-22 12:04:53 -0300

    Add files via upload

--- pasta de trabalhao objetivos/nome.js
+++ pasta de trabalhao objetivos/nome.js
@@ -1,4 +1,5 @@
 const botoes = document.querySelectorAll(".botao");
+const texto=document.querySelectorAll("aba-conteudo");
 //Declara quais elementos vão ser lidos. O script vai ler todos que tiverem a classe "botao".
 
 for(let i=0;i <botoes.length;i++){
@@ -6,9 +7,12 @@ for(let i=0;i <botoes.length;i++){
     botoes[i].onclick = function() {
         for(let j=0;j<botoes.length;j++) {
         botoes[j].classList.remove("ativo");
+        texto[j].classList.remove("ativo");
         }
         //Remove a classe "ativo" de todos os botões.
         botoes[i].classList.add("ativo");
+        texto[i].classList.add("ativo");
+
         //Adiciona a classe ativo só no botão clicado.
     }
 }
